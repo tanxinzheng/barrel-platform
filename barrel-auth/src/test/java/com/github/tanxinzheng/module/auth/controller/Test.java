57cb645d3ddba2c3e83d564ea7cd658941d11b6a
tanxinzheng <tanxinzheng@139.com>
2020-07-08 15:01:42 +0800

    支持根据profile环境配置化打包

---
+++ barrel-auth/src/test/java/com/github/tanxinzheng/module/auth/controller/Test.java
@@ -0,0 +1,11 @@
+package com.github.tanxinzheng.module.auth.controller;
+
+import com.github.tanxinzheng.framework.utils.UUIDGenerator;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String uuid = UUIDGenerator.getInstance().getUUID();
+        System.out.println(uuid);
+    }
+}
