a02052cba19d6e2e1483520f5664205c362cbd7b
tanxinzheng <tanxinzheng@139.com>
2020-11-30 21:01:42 +0800

    继承sleuth模块

---
+++ barrel-tools/src/test/java/com/github/tanxinzheng/tools/process/RedisConfig.java
@@ -0,0 +1,99 @@
+package com.github.tanxinzheng.tools.process;
+
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.github.tanxinzheng.cache.serializer.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Resource
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Bean
+    @Primary//当有多个管理器的时候，必须使用该注解在一个管理器上注释：表示该管理器为默认的管理器
+    public CacheManager cacheManager() {
+        //初始化RedisCacheManager
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(lettuceConnectionFactory);
+        builder.cacheDefaults(redisCacheConfiguration());
+        //设置白名单---非常重要********
+        /*
+        使用fastjson的时候：序列化时将class信息写入，反解析的时候，
+        fastjson默认情况下会开启autoType的检查，相当于一个白名单检查，
+        如果序列化信息中的类路径不在autoType中，
+        反解析就会报com.alibaba.fastjson.JSONException: autoType is not support的异常
+        可参考 https://blog.csdn.net/u012240455/article/details/80538540
+         */
+        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        return builder.build();
+    }
+
+    /**
+     *  设置 redis 数据默认过期时间
+     *  设置@cacheable 序列化方式
+     * @return
+     */
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(){
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration
+                // 使用fastjson序列化
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer))
+                // 默认30秒过期
+                .entryTtl(Duration.ofDays(30));
+        return configuration;
+    }
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    @Bean(name = "redisTemplate")
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        //使用fastjson序列化
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(fastJsonRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        // key的序列化采用StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setConnectionFactory(lettuceConnectionFactory);
+        return template;
+    }
+
+}
