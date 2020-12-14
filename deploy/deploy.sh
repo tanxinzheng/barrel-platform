d9d386ad6bf1001639d0a7cf745b4be257e30a10
tanxinzheng <tanxinzheng@139.com>
2020-12-14 18:02:18 +0800

    docker-compose.yml优化

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,16 +3,16 @@ export RUN_AS_USER=admin
 export BASH_ENV=/etc/profile
 echo $PATH
 rm -rf barrel-framework
-rm -rf barrel-plateform
+rm -rf barrel-platform
 git clone -b 1.0.0 https://gitee.com/tanxinzheng/barrel-framework.git
 git clone -b 1.0.0 https://gitee.com/tanxinzheng/barrel-platform.git
 
-mvn install -f barrel-framework/pom.xml
-mvn install -f barrel-plateform/pom.xml
+mvn clean install -f barrel-framework/pom.xml
+mvn clean install -f barrel-platform/pom.xml
 
-tar -zxvf barrel-plateform/barrel-system/target/barrel-system.tar.gz -C /app/deploy
-tar -zxvf barrel-plateform/barrel-gateway/target/barrel-gateway.tar.gz -C /app/deploy
-tar -zxvf barrel-plateform/barrel-service/barrel-module-system/target/barrel-system.tar.gz -C /app/deploy
+tar -zxvf barrel-platform/barrel-auth/target/barrel-auth.tar.gz -C /app/deploy
+tar -zxvf barrel-platform/barrel-gateway/target/barrel-gateway.tar.gz -C /app/deploy
+tar -zxvf barrel-platform/barrel-service/barrel-module-system/target/barrel-system.tar.gz -C /app/deploy
 
 echo "Restart Barrel-Auth Start...."
 sudo systemctl restart barrel-system
