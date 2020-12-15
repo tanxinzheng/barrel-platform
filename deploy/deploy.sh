187b57c22a6058b40a9ef2a375ac098481df3650
tanxinzheng <tanxinzheng@139.com>
2020-12-15 19:00:07 +0800

    移除docker-compose中的barrel-system模块

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,15 +12,10 @@ mvn clean install -f barrel-platform/pom.xml
 
 tar -zxf barrel-platform/barrel-auth/target/barrel-auth.tar.gz -C /app/deploy
 tar -zxf barrel-platform/barrel-gateway/target/barrel-gateway.tar.gz -C /app/deploy
-tar -zxf barrel-platform/barrel-service/barrel-module-system/target/barrel-system.tar.gz -C /app/deploy
 
 echo "Restart Barrel-Auth Start...."
-sudo systemctl restart barrel-system
+sudo systemctl restart barrel-auth
 echo "Restart Barrel-Auth End...."
-
-echo "Restart Barrel-System Start...."
-sudo systemctl restart barrel-system
-echo "Restart Barrel-System End...."
 
 echo "Restart Barrel-Gateway Start...."
 sudo systemctl restart barrel-gateway
